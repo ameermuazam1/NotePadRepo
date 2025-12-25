b1e54df69db7725d5091ea51a261dfdb47977882
ameermuazam1 <ameer1muazam@gmail.com>
2025-12-25 22:40:44 +0500

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace NotePad
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.OpenFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.NewFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.OpenFileFromPc = new System.Windows.Forms.ToolStripMenuItem();
+            this.SsaveAss = new System.Windows.Forms.ToolStripMenuItem();
+            this.Save = new System.Windows.Forms.ToolStripMenuItem();
+            this.Exit = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.Undo = new System.Windows.Forms.ToolStripMenuItem();
+            this.Cut = new System.Windows.Forms.ToolStripMenuItem();
+            this.Copy = new System.Windows.Forms.ToolStripMenuItem();
+            this.Paste = new System.Windows.Forms.ToolStripMenuItem();
+            this.SelectAll = new System.Windows.Forms.ToolStripMenuItem();
+            this.AllNote = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.editToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.OpenFile,
+            this.NewFile,
+            this.OpenFileFromPc,
+            this.SsaveAss,
+            this.Save,
+            this.Exit});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // OpenFile
+            // 
+            this.OpenFile.Name = "OpenFile";
+            this.OpenFile.Size = new System.Drawing.Size(180, 22);
+            this.OpenFile.Text = "Open";
+            this.OpenFile.Click += new System.EventHandler(this.OpenFile_Click);
+            // 
+            // NewFile
+            // 
+            this.NewFile.Name = "NewFile";
+            this.NewFile.Size = new System.Drawing.Size(180, 22);
+            this.NewFile.Text = "New File";
+            this.NewFile.Click += new System.EventHandler(this.NewFile_Click);
+            // 
+            // OpenFileFromPc
+            // 
+            this.OpenFileFromPc.Name = "OpenFileFromPc";
+            this.OpenFileFromPc.Size = new System.Drawing.Size(180, 22);
+            this.OpenFileFromPc.Text = "Open File";
+            this.OpenFileFromPc.Click += new System.EventHandler(this.OpenFileFromPc_Click);
+            // 
+            // SsaveAss
+            // 
+            this.SsaveAss.Name = "SsaveAss";
+            this.SsaveAss.Size = new System.Drawing.Size(180, 22);
+            this.SsaveAss.Text = "Save As";
+            this.SsaveAss.Click += new System.EventHandler(this.SsaveAss_Click);
+            // 
+            // Save
+            // 
+            this.Save.Name = "Save";
+            this.Save.Size = new System.Drawing.Size(180, 22);
+            this.Save.Text = "Save";
+            this.Save.Click += new System.EventHandler(this.Save_Click_1);
+            // 
+            // Exit
+            // 
+            this.Exit.Name = "Exit";
+            this.Exit.Size = new System.Drawing.Size(180, 22);
+            this.Exit.Text = "Exit";
+            this.Exit.Click += new System.EventHandler(this.Exit_Click);
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.Undo,
+            this.Cut,
+            this.Copy,
+            this.Paste,
+            this.SelectAll});
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.editToolStripMenuItem.Text = "Edit";
+            this.editToolStripMenuItem.Click += new System.EventHandler(this.editToolStripMenuItem_Click);
+            // 
+            // Undo
+            // 
+            this.Undo.Name = "Undo";
+            this.Undo.Size = new System.Drawing.Size(185, 22);
+            this.Undo.Text = "Undo";
+            this.Undo.Click += new System.EventHandler(this.Undo_Click);
+            // 
+            // Cut
+            // 
+            this.Cut.Name = "Cut";
+            this.Cut.Size = new System.Drawing.Size(185, 22);
+            this.Cut.Text = "Cut";
+            this.Cut.Click += new System.EventHandler(this.Cut_Click);
+            // 
+            // Copy
+            // 
+            this.Copy.Name = "Copy";
+            this.Copy.Size = new System.Drawing.Size(185, 22);
+            this.Copy.Text = "Copy";
+            this.Copy.Click += new System.EventHandler(this.Copy_Click);
+            // 
+            // Paste
+            // 
+            this.Paste.Name = "Paste";
+            this.Paste.Size = new System.Drawing.Size(185, 22);
+            this.Paste.Text = "Paste";
+            this.Paste.Click += new System.EventHandler(this.Paste_Click);
+            // 
+            // SelectAll
+            // 
+            this.SelectAll.Name = "SelectAll";
+            this.SelectAll.Size = new System.Drawing.Size(185, 22);
+            this.SelectAll.Text = "Select All/DeselectAll";
+            this.SelectAll.Click += new System.EventHandler(this.SelectAll_Click);
+            // 
+            // AllNote
+            // 
+            this.AllNote.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.AllNote.Location = new System.Drawing.Point(0, 24);
+            this.AllNote.Multiline = true;
+            this.AllNote.Name = "AllNote";
+            this.AllNote.Size = new System.Drawing.Size(800, 426);
+            this.AllNote.TabIndex = 1;
+            this.AllNote.MouseClick += new System.Windows.Forms.MouseEventHandler(this.AllNote_MouseClick);
+            this.AllNote.TextChanged += new System.EventHandler(this.AllNote_TextChanged);
+            this.AllNote.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.AllNote_KeyPress);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.AllNote);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem OpenFile;
+        private System.Windows.Forms.ToolStripMenuItem NewFile;
+        private System.Windows.Forms.ToolStripMenuItem OpenFileFromPc;
+  //      private System.Windows.Forms.ToolStripMenuItem SaveAs;
+        private System.Windows.Forms.TextBox AllNote;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem Undo;
+        private System.Windows.Forms.ToolStripMenuItem Cut;
+        private System.Windows.Forms.ToolStripMenuItem Copy;
+        private System.Windows.Forms.ToolStripMenuItem Paste;
+        private System.Windows.Forms.ToolStripMenuItem SelectAll;
+        private System.Windows.Forms.ToolStripMenuItem Save;
+        private System.Windows.Forms.ToolStripMenuItem Exit;
+        private System.Windows.Forms.ToolStripMenuItem SsaveAss;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace NotePad
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            ChechEditorText();
+            TextCopied();
+
+        }
+        string Fpath = ""; //File Path of text
+        bool textChanged = false;
+        bool isAllTextSelected = false;
+        bool isCopiedText = false;
+        private object openFileDialog1;
+
+        private void OpenFile_Click(object sender, EventArgs e)
+        {
+            AllNote.Clear();
+            Fpath = "";
+        }
+
+        private void NewFile_Click(object sender, EventArgs e)
+        {
+            Process.Start(Application.ExecutablePath);
+        }
+
+        private void OpenFileFromPc_Click(object sender, EventArgs e)
+        {
+
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+
+                AllNote.Text = File.ReadAllText(openFileDialog1.FileName);
+                Fpath = openFileDialog1.FileName;
+            }
+        }
+       
+        private void SaveAs()
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                Fpath = saveFileDialog.FileName;
+                File.WriteAllText(Fpath, AllNote.Text); // give path and text
+            }
+        }
+        private void SsaveAss_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                Fpath = saveFileDialog.FileName;
+                File.WriteAllText(Fpath, AllNote.Text);
+            }
+        }
+
+        private void Save_Click_1(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(Fpath))
+            {
+                SaveAs();
+            }
+            else
+            {
+                File.WriteAllText(Fpath, AllNote.Text);
+            }
+        }
+
+        private void AllNote_TextChanged(object sender, EventArgs e)
+        {
+            textChanged = true;
+            if (AllNote.Text == string.Empty)
+            {
+                textChanged = false;
+            }
+        }
+
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            if (textChanged)
+            {
+                DialogResult da = MessageBox.Show("Do you want to save your text ?", "Warning", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Warning);
+                if (da == DialogResult.Yes)
+                    {
+                    Save_Click_1(sender, e);
+                }
+                else if (da == DialogResult.Cancel)
+                {
+                    return;
+                }
+            }
+            Application.Exit();
+        }
+
+        private void Undo_Click(object sender, EventArgs e)
+        {
+            if (AllNote.CanUndo)
+                AllNote.Undo();
+        }
+
+        private void Cut_Click(object sender, EventArgs e)
+        {
+            AllNote.Cut();
+            isCopiedText = true;
+        }
+        private void Paste_Click(object sender, EventArgs e)
+        {
+            if (isCopiedText)
+                AllNote.Paste();
+
+        }
+        private void Copy_Click(object sender, EventArgs e)
+        {
+            if (isCopiedText)
+                AllNote.Paste();
+        }
+
+        private void SelectAll_Click(object sender, EventArgs e)
+        {
+            if (!isAllTextSelected)
+            {
+                AllNote.SelectAll(); // Select All
+                isAllTextSelected = true;
+            }
+            else
+            {
+                AllNote.DeselectAll(); // DeSelect All
+                isAllTextSelected = false;
+            }
+
+        }
+
+        private void AllNote_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == '\u0001' && !string.IsNullOrEmpty(AllNote.Text))
+            {
+                isAllTextSelected = true;
+            }
+
+        }
+        public void ChechEditorText()
+        {
+            if (!string.IsNullOrEmpty(AllNote.Text) && AllNote.SelectedText.Count() > 0)
+            {
+                Copy.Enabled = true;
+                Cut.Enabled = true;
+                Exit.Enabled = true;
+                SelectAll.Enabled = true;
+            }
+
+            if (string.IsNullOrEmpty(AllNote.Text))
+            {
+                SelectAll.Enabled = false;
+            }
+            else
+            {
+                SelectAll.Enabled = true;
+            }
+
+            if (string.IsNullOrEmpty(AllNote.Text) || AllNote.SelectedText.Count() == 0)
+            {
+                Copy.Enabled = false;
+                Cut.Enabled = false;
+                Exit.Enabled = false;
+            }
+            //  selectAllToolStripMenuItem1.Enabled = false;
+        }
+
+   
+     public void TextCopied()
+        {
+            if (Clipboard.ContainsText())
+            {
+                isCopiedText = true;
+                Paste.Enabled = true;
+                return;
+            }
+            Paste.Enabled = false;
+        }
+
+        private void AllNote_MouseClick(object sender, MouseEventArgs e)
+        {
+            ChechEditorText();
+            TextCopied();
+        }
+
+        private void editToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            TextCopied();
+        }
+
+       
+    }
+}
